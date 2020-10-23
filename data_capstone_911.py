f36d302b9533982eb14cde4e1b7ebfab91984f94
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-10-23 10:44:32 +0200

    data capstone

--- data_capstone_911.py
+++ data_capstone_911.py
@@ -23,8 +23,64 @@ print(df['title'].nunique())
 
 df['Reason'] = df['title'].apply(lambda title: title.split(':')[0])
 print(df['Reason'].value_counts())
-sns.countplot(x='Reason', data=df)
-plt.show()
+sns.countplot(x='Reason', data=df, palette='coolwarm')
 
 print(type(df['timeStamp'].iloc[0]))
 df['timeStamp'] = pd.to_datetime(df['timeStamp'])
+df['Hour'] = df['timeStamp'].apply(lambda time: time.hour)
+df['Month'] = df['timeStamp'].apply(lambda time: time.month)
+df['Day of Week'] = df['timeStamp'].apply(lambda time: time.dayofweek)
+
+dmap = {0:'Mon',
+        1:'Tue',
+        2:'Wed',
+        3:'Thu',
+        4:'Fri',
+        5:'Sat',
+        6:'Sun'}
+df['Day of Week'] = df['Day of Week'].map(dmap)
+
+sns.set_style('whitegrid')
+sns.countplot(x='Day of Week', hue='Reason', data=df, palette='viridis')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+sns.countplot(x='Month', hue='Reason', data=df, palette='viridis')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
+
+byMonth = df.groupby('Month').count()
+
+sns.lmplot(x='Month', y='twp', data=byMonth.reset_index())
+
+df['Date']=df['timeStamp'].apply(lambda t:t.date())
+
+byDate = df.groupby('Date').count()['twp'].plot()
+plt.tight_layout()
+
+df[df['Reason']=='Traffic'].groupby('Date').count()['twp'].plot()
+plt.title('Traffic')
+plt.tight_layout()
+
+df[df['Reason']=='Fire'].groupby('Date').count()['twp'].plot()
+plt.title('Fire')
+plt.tight_layout()
+
+df[df['Reason']=='EMS'].groupby('Date').count()['twp'].plot()
+plt.title('EMS')
+plt.tight_layout()
+
+dayHour = df.groupby(by=['Day of Week','Hour']).count()['Reason'].unstack()
+dayHour.head()
+
+plt.figure(figsize=(12,6))
+sns.heatmap(dayHour,cmap='viridis')
+
+sns.clustermap(dayHour, cmap='viridis')
+
+dayMonth = df.groupby(by=['Day of Week','Month']).count()['Reason'].unstack()
+dayMonth.head()
+
+plt.figure(figsize=(12,6))
+sns.heatmap(dayMonth,cmap='viridis')
+
+
